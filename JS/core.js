455038ab3e96571a018263640e7c7af21a6c9b54
Arthur <Arthur>
2016-09-20 21:13:01 -0300

    Feito: - Warp/Loop dos veículos - Pontuação e mensagens - Pontuação por mapa finalizado ou nova linha andada - Restrição de movimento do jogador - Detecção básicad e colisão  Falta: - Detecção precisa de colisão - Mensagens mais claras ao usuário Nice to Have: - Gerar posição dos inimigos de forma aleatória - Adicionar sprites

--- JS/core.js
+++ JS/core.js
@@ -1,7 +1,17 @@
 var sapo, pontos, vidas;
+var timer = new Date().getTime();//Utilizado para medir e limtiar o tempo entre entradas do usuário
+var typeOfComponent = {
+    Veiculo : 1,
+    Sapo : 2,
+    Mapa : 0,
+    properties: {
+        1 : {warp: true}, 
+        2 : {warp: false},
+        0 : {warp: false},
+    }
+}
 
-
-function componentes(largura, altura, cor, x, y) {
+function componentes(largura, altura, cor, x, y, tipoDeComponente) {
     this.gamearea = telaDoJogo;
     this.width = largura;
     this.height = altura;
@@ -9,15 +19,58 @@ function componentes(largura, altura, cor, x, y) {
     this.speedY = 0;    
     this.x = x;
     this.y = y;    
-    
+    this.initialX = this.x;
+    this.initialY = this.y;
+    this.tipo = tipoDeComponente;
+    this.biggestY = this.initialY;
+
     this.atualizar = function() {
         context = telaDoJogo.context;
         context.fillStyle = cor;
         context.fillRect(this.x, this.y, this.width, this.height);
     }
     this.novaPos = function() {
-        this.x += this.speedX;
-        this.y += this.speedY;        
+        if(typeOfComponent.properties[this.tipo].warp){
+            if(this.x > telaDoJogo.canvas.width  || this.x < 0)
+            {
+                this.x = this.initialX;
+            }
+            else{
+                this.x += this.speedX;  
+                
+            }
+            if(this.y  > telaDoJogo.canvas.height || this.y < 0 )
+            {
+                this.y = this.initialY;
+            }
+            else{
+                this.y += this.speedY;
+            }
+            if(sapo.x === this.x && sapo.y === this.y){
+                    perder();
+            }
+        }
+        else if(this.tipo == typeOfComponent.Sapo){
+            if(this.x + this.speedX < telaDoJogo.canvas.width && this.x + this.speedX > 0)//Limita o movimento do sapo a area do mapa
+            {
+                this.x += this.speedX;
+            }
+            if(this.y + this.speedY < telaDoJogo.canvas.height && this.y + this.speedY > 0 )//Limita o movimento do sapo a area do mapa
+            {
+                this.y += this.speedY;
+                if(this.y < this.biggestY){
+                    this.biggestY = this.y;
+                    adicionarPontuacao(10);
+                }
+            }
+            else{ // Caso ele atinja a parte superior do mapa vence o jogo
+                vencer(); //Venceu a fase
+            }
+        }
+        else{
+            this.x += this.speedX;
+            this.y += this.speedY;   
+        }     
     }
 }
 
@@ -27,23 +80,23 @@ function atualizaTeladeJogo() {
     sapo.speedY = 0;    
     carro1.speedX = +10;
     carro1.novaPos()
-    carro2.speedX = -20;
+    carro2.speedX = -10;
     carro2.novaPos()
-    moto.speedX = +30;
+    moto.speedX = +10;
     moto.novaPos()
     
-         
-    if (telaDoJogo.key && telaDoJogo.key == 37) {sapo.speedX = -1; }
-    if (telaDoJogo.key && telaDoJogo.key == 39) {sapo.speedX = 1; }
-    if (telaDoJogo.key && telaDoJogo.key == 38) {sapo.speedY = -1; }
-    if (telaDoJogo.key && telaDoJogo.key == 40) {sapo.speedY = 1; }
-    sapo.novaPos();    
-
+    if( new Date().getTime() - timer > 150 ){ // Limita o tempo entre entradas do usuário
+        if (telaDoJogo.key && telaDoJogo.key == 37) {sapo.speedX = -30; timer =  new Date().getTime();}
+        if (telaDoJogo.key && telaDoJogo.key == 39) {sapo.speedX = 30; timer =  new Date().getTime();}
+        if (telaDoJogo.key && telaDoJogo.key == 38) {sapo.speedY = -30; timer =  new Date().getTime();}
+        if (telaDoJogo.key && telaDoJogo.key == 40) {sapo.speedY = 30; timer =  new Date().getTime();}
+        sapo.novaPos();    
+    }
     areasegura1.atualizar();
     areasegura2.atualizar();
     
     sapo.atualizar();
-    
+
     carro1.atualizar()
     carro2.atualizar()
     moto.atualizar()
@@ -74,17 +127,68 @@ var telaDoJogo = {
     }
 }
 
+function vencer(){
+    exibirMensagem('Venceu!');
+    adicionarPontuacao(100);
+    reiniciarJogo(false);
+
+}
+
+function perder(){
+    exibirMensagem('Perdeu!');
+    reiniciarJogo(true);
+}
+
+function reiniciarJogo(zerarPontos){
+    //Reseta o jogo
+    telaDoJogo.clear();
+    iniciarComponentes();
+    if(zerarPontos){
+        zerarPontuacao();
+    }
+}
+
+function exibirMensagem(mensagem){
+    document.getElementById('Mensagem').innerText = mensagem;
+    document.getElementById('Mensagem').setAttribute('visible',true);
+}
+
+function adicionarPontuacao(pontos){
+    var temp = parseInt(document.getElementById('score').innerText) + parseInt(pontos);
+    var comprimentoFaltanteNumero = 5 -  String(temp).length;
+    var numeroFinal = '';
+    for(i = 0 ; i < comprimentoFaltanteNumero ; i ++){
+        numeroFinal += '0';
+    }
+    numeroFinal += String(temp);
+    document.getElementById('score').innerText = numeroFinal;
+}
+
+function zerarPontuacao(pontos){
+    var temp = 0;
+    var comprimentoFaltanteNumero = 5 -  String(temp).length;
+    var numeroFinal = '';
+    for(i = 0 ; i < comprimentoFaltanteNumero ; i ++){
+        numeroFinal += '0';
+    }
+    numeroFinal += String(temp);
+    document.getElementById('score').innerText = numeroFinal;
+}
+
+function iniciarComponentes(){
+    carro1 =  new componentes(180, 85, "black", 0, 65, typeOfComponent.Veiculo);
+    carro2 =  new componentes(120, 85, "red", 600, 250, typeOfComponent.Veiculo);
+    moto =  new componentes(100, 30, "blue", 0, 200, typeOfComponent.Veiculo);
+    
+    sapo = new componentes(30, 30, "#32CD32", 320, 370, typeOfComponent.Sapo);
+    
+    areasegura1 =  new componentes(640, 60, "#90EE90", 0, 0, typeOfComponent.Mapa);
+    areasegura2 =  new componentes(640, 32, "#90EE90", 0, 370, typeOfComponent.Mapa);
+}
 
 window.onload = function () {
     
-    carro1 =  new componentes(180, 85, "gray", 60, 65);
-    carro2 =  new componentes(120, 85, "red", 500, 250);
-    moto =  new componentes(100, 30, "blue", 320, 200);
-    
-    sapo = new componentes(30, 30, "#006010", 320, 370);
-    
-    areasegura1 =  new componentes(640, 60, "green", 0, 0);
-    areasegura2 =  new componentes(640, 32, "green", 0, 370);
+    iniciarComponentes();
     
     telaDoJogo.start();
 }
